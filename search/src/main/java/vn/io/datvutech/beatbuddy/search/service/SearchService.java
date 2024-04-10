5855778027dc805a2d43880f013e3b9bf1cfd624
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-04-10 06:08:02 +0000

    fix: update git modules

--- search/src/main/java/vn/io/datvutech/beatbuddy/search/service/SearchService.java
+++
@@ -1,7 +0,0 @@
-package vn.io.datvutech.beatbuddy.search.service;
-
-import vn.io.datvutech.beatbuddy.search.dto.TrackDto;
-
-public interface SearchService {
-    void createTrack(TrackDto trackDto);
-}
